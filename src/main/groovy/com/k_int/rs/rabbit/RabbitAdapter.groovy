8721576b38aaca7c1aea990a6f2fb9589393501f
Ian Ibbotson <ian.ibbotson@gmail.com>
2018-02-04 17:04:33 +0000

    Use annotations to configure service entry

--- src/main/groovy/com/k_int/rs/rabbit/RabbitAdapter.groovy
+++ src/main/groovy/com/k_int/rs/rabbit/RabbitAdapter.groovy
@@ -1,5 +1,9 @@
 package com.k_int.rs.rabbit
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RabbitAdapter {
 
   public void start() {
