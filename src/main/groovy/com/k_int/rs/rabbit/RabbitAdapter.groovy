f91774bc86139468dd34dc9749826100d30e9812
Ian Ibbotson <ianibbo@gmail.com>
2018-02-05 10:31:10 +0000

    Working parse of json payload

--- src/main/groovy/com/k_int/rs/rabbit/RabbitAdapter.groovy
+++ src/main/groovy/com/k_int/rs/rabbit/RabbitAdapter.groovy
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javax.annotation.PostConstruct
+import groovy.json.JsonSlurper
 
 /**
  *
@@ -23,8 +24,20 @@ public class RabbitAdapter {
 
 
   public void receiveMessage(byte[] message) {
-    String json_payload = new String(message);
-    logger.debug("RabbitAdapter::receiveMessage() ${json_payload}");
+    String json_payload_as_string = new String(message);
+    try {
+      def jsonSlurper = new JsonSlurper()
+      def parsed_message = jsonSlurper.parseText(json_payload_as_string)
+
+      logger.debug("RabbitAdapter::receiveMessage()");
+      logger.debug("Parsed json: ${parsed_message}");
+    }
+    catch ( Exception e ) {
+      logger.error("Problem parsing JSON payload",e);
+    }
+    finally {
+      logger.debug('RabbitAdapter::receiveMessage complete');
+    }
   }
 
 }
--- src/test/scripts/send_req.sh
+++ src/test/scripts/send_req.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
-rabbitmqadmin publish exchange=RSExchange routing_key=OutViaProtocol.TCP payload="{
-  header:{
-    address:'localhost',
-    port:499
+rabbitmqadmin publish exchange=RSExchange routing_key=OutViaProtocol.TCP payload="
+{
+  \"header\":{
+    \"address\":\"localhost\",
+    \"port\":499
   },
-  message:{
-    request:{
-      protocol_version_num:1,
-      transaction_id:{
-        // Transaction_Id_type
-        transaction_group_qualifier:\"A-Random-TGQ\",
-        transaction_qualifier:\"A-Random-TQ\"
+  \"message\":{
+    \"request\":{
+      \"protocol_version_num\":1,
+      \"transaction_id\":{
+        \"transaction_group_qualifier\":\"A-Random-TGQ\",
+        \"transaction_qualifier\":\"A-Random-TQ\"
       },
-      service_date_time: {
-        date_time_of_this_service:{date:\"20170101\", time:\"0000\"},
-        date_time_of_original_service:{date:\"20180101\",time:\"1111\"},
+      \"service_date_time\": {
+        \"date_time_of_this_service\":{\"date\":\"20170101\", \"time\":\"0000\"},
+        \"date_time_of_original_service\":{\"date\":\"20180101\",\"time\":\"1111\"},
       },
-      transaction_type:1,
-      iLL_service_type:[\"loan\",\"copy-non-returnable\",\"locations\",\"estimate\",\"responder-specific\"],
-      requester_optional_messages:{
-        can_send_RECEIVED:true,
-        can_send_RETURNED:true,
-        requester_SHIPPED:2,
-        requester_CHECKED_IN:2,
+      \"requester_id\":{
+        \"person_or_institution_symbol\":{
+	  \"institution_symbol\":\"ILLTEST-local-001\"
+	}
       },
-      place_on_hold: 3,
-      item_id:{
-        title:\"A test title\"
+      \"responder_id\":{
+        \"person_or_institution_symbol\":{
+	  \"institution_symbol\":\"ILLTEST-local-002\"
+	}
       },
-      retry_flag:false,
-      forward_flag:false
+      \"transaction_type\":1,
+      \"iLL_service_type\":[\"loan\",\"copy-non-returnable\",\"locations\",\"estimate\",\"responder-specific\"],
+      \"requester_optional_messages\":{
+        \"can_send_RECEIVED\":true,
+        \"can_send_RETURNED\":true,
+        \"requester_SHIPPED\":2,
+        \"requester_CHECKED_IN\":2,
+      },
+      \"place_on_hold\": 3,
+      \"item_id\":{
+        \"title\":\"A test title\"
+      },
+      \"retry_flag\":false,
+      \"forward_flag\":false,
+      \"requester_note\":\"ILLTEST-CASE-001\"
     }
   }
 }"
