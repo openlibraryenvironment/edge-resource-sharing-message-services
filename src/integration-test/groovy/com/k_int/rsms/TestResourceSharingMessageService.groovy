b0a4c2b28b59ee0dcc4a58c43a504acdc3bb07c6
Ian Ibbotson <ianibbo@gmail.com>
2018-02-09 10:56:48 +0000

    integration test runs

--- src/integration-test/groovy/com/k_int/rsms/TestResourceSharingMessageService.groovy
+++ src/integration-test/groovy/com/k_int/rsms/TestResourceSharingMessageService.groovy
@@ -4,13 +4,23 @@ import spock.lang.Specification
 import spock.lang.Unroll
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.k_int.rs.server.RSServer
 
+import org.junit.runner.RunWith;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+// @TestPropertySource( locations = "classpath:application-integrationtest.properties")
+
+@RunWith(SpringRunner.class)
+@SpringBootTest( classes = RSServer.class)
 class TestResourceSharingMessageService  extends Specification {
 
   final static Logger logger = LoggerFactory.getLogger(TestResourceSharingMessageService.class);
 
-  @Unroll
+  @Test
   def simpleTest() {
     when:
       def i=1;
