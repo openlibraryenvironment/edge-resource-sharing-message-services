687b269a1735cdbe1fdcec3237a4f0ee904512f0
Ian Ibbotson <ianibbo@gmail.com>
2019-02-17 15:06:15 +0000

    More work on init scripts

--- src/main/deb/scripts/rs_message_service.sh
+++ src/main/deb/scripts/rs_message_service.sh
@@ -4,6 +4,30 @@
 
 echo Start RS message service
 
+# Look for Java and check version
+java_check() {
+   if [ -n "$JAVA_HOME" -a -x "$JAVA_HOME/bin/java" ]; then
+      JAVA="$JAVA_HOME/bin/java"
+   else
+      JAVA=`which java`
+   fi
+
+   if [ ! -x "$JAVA" ]; then
+      echo "Could not find any executable java binary."
+      echo "Install java in your PATH or set JAVA_HOME"
+      exit 1
+   else
+      VERSION=$("$JAVA" -version 2>&1 | awk -F '"' '/version/ { print $2 }' \
+              | awk -F '.' '{ print $2 }')
+      if [ "$VERSION" -lt "8" ];  then
+         echo "Java version 8 or higher is required."
+         exit 0
+      fi
+   fi
+}  # end java_check
+
+java_check
+
 if [ -f "${PID_DIR}/rs_message_service.pid" ]; then
    PID=`cat ${PID_DIR}/rs_message_service.pid`
    if ps -p $PID > /dev/null; then
@@ -15,19 +39,19 @@ if [ -f "${PID_DIR}/rs_message_service.pid" ]; then
       exit 1
    fi
 else
+   RS_MESSAGE_SERVICE_JAR="/opt/rs-message-service/lib/resource-sharing-message-services-1.0.jar"
 
    echo ""
    echo "############################"
    echo "JAVA: $JAVA $VERSION"
    echo "JVM OPTIONS: $RS_MESSAGE_SERVICE_JAVA_OPTS"
-   echo "RS_MESSAGE_SERVICE ROLE: $role"
    echo "RS_MESSAGE_SERVICE OPTIONS: $RS_MESSAGE_SERVICE_OPTIONS"
    echo "RS_MESSAGE_SERVICE JAR: $RS_MESSAGE_SERVICE_JAR"
    echo "############################"
    echo ""
 
-   echo -n "Starting Okapi..."
-   exec $JAVA $OKAPI_JAVA_OPTS -jar "$OKAPI_JAR" $role $OKAPI_OPTIONS <&- &
+   echo -n "Starting RS Message services..."
+   exec $JAVA $RS_MESSAGE_SERVICE_JAVA_OPTS -jar "$RS_MESSAGE_SERVICE_JAR" $RS_MESSAGE_SERVICE_OPTIONS <&- &
 
    RETVAL=$?
    PID=$!
