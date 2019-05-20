c2f50464d26b5e66480045d277b3cea466577e69
Ian Ibbotson <ianibbo@googlemail.com>
2019-05-20 14:33:14 +0100

    Incoming iso18626 messages are placed on the incoming request message queue with a routing key that identifies the symbol of the message receipient

--- src/main/groovy/com/k_int/rs/server/Iso18626Controller.groovy
+++ src/main/groovy/com/k_int/rs/server/Iso18626Controller.groovy
@@ -8,16 +8,14 @@ import org.springframework.http.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.olf.reshare.iso18626.schema.*;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import org.olf.reshare.iso18626.ISO18626ToJsonDataBinder;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
 
 // See https://docs.spring.io/spring-boot/docs/current/reference/html/howto-spring-mvc.html
 // also https://www.baeldung.com/spring-request-response-body
@@ -29,6 +27,14 @@ public class Iso18626Controller {
 
   private final Logger logger = LoggerFactory.getLogger(Iso18626Controller.class);
 
+  private RabbitTemplate rabbitTemplate;
+
+  @Autowired
+  public Iso18626Controller(RabbitTemplate rabbitTemplate) {
+    this.rabbitTemplate = rabbitTemplate;
+  }
+
+
   @RequestMapping(value='/iso18626', method=RequestMethod.POST, consumes=MediaType.APPLICATION_XML_VALUE)
   public ISO18626Message isoResponse(HttpServletRequest request,
                             @RequestBody ISO18626Message iso_18626_message) {
@@ -37,12 +43,13 @@ public class Iso18626Controller {
     // logger.debug("As JSON ${message_data}");
     ISO18626Message result = null;
 
-    String target_tenant = resolveRecipient(message_data);
+    String message_recipient_symbol = resolveRecipient(message_data);
 
-    if ( target_tenant ) {
+    if ( message_recipient_symbol ) {
       // Generate an error confirmation
-      logger.debug("Enqueue message for tenant ${target_tenant}");
+      logger.debug("Enqueue message for recipient symbol ${message_recipient_symbol}");
       result = generateConfirmation(iso_18626_message);
+      rabbitTemplate.convertAndSend('RSExchange', 'RSInboundMessage.ISO18626.'+message_recipient_symbol, message_data);
     }
     else {
       logger.error("Unable to resolve tenant based on message payload");
@@ -62,6 +69,7 @@ public class Iso18626Controller {
     }
     else {
       // ERROR
+      logger.error("Unhandled message type");
     }
 
     return result;
@@ -83,8 +91,10 @@ public class Iso18626Controller {
     if ( request_data?.request ) {
       logger.debug("Processing a request message - look in supplyingAgencyId for the intended recipient info");
       // We have been sent a request - this means that the intended recipient can be found in the supplyingAgencyId of the header
-      result = resolveTenant(request_data?.request?.header?.supplyingAgencyId?.agencyIdType?.value,
-                             request_data?.request?.header?.supplyingAgencyId?.agencyIdValue);
+      // result = resolveTenant(request_data?.request?.header?.supplyingAgencyId?.agencyIdType?.value,
+      //                        request_data?.request?.header?.supplyingAgencyId?.agencyIdValue);
+      result = request_data?.request?.header?.supplyingAgencyId?.agencyIdType?.value+':'+
+               request_data?.request?.header?.supplyingAgencyId?.agencyIdValue;
     }
     else {
       logger.error("Unhandled request type in ${request_data}");
